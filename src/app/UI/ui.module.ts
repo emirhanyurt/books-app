d5ce2c181af8ca2878bdc810b8138db079c239b8
Emirhan YURT <emirhanyurt.py@gmail.com>
2022-10-25 11:53:15 +0300

    guard added

--- src/app/UI/ui.module.ts
+++ src/app/UI/ui.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutModule } from './layout/layout.module';
 import { ComponentModule } from './components/component.module';
+import { LoginModule } from './login/login/login.module';
 
 
 
@@ -10,11 +11,13 @@ import { ComponentModule } from './components/component.module';
   imports: [
     CommonModule,
     LayoutModule,
+    LoginModule,
     ComponentModule
   ],
   exports:[
     LayoutModule,
-    ComponentModule
+    ComponentModule,
+    LoginModule
   ]
 })
 export class UiModule { }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { BooksComponent } from './UI/components/books/books.component';
 import { HomeComponent } from './UI/components/home/home.component';
 import { LayoutComponent } from './UI/layout/layout/layout.component';
+import { AuthGuard } from './UI/login/login/guard/auth.guard';
+import { LoginComponent } from './UI/login/login/login.component';
 
 const routes: Routes = [
-  {path:'',component:LayoutComponent, children:[
+  {path:'',component:LayoutComponent,canActivateChild: [AuthGuard], children:[
     {
     path:'',component:HomeComponent,
     loadChildren: () => import('./UI/components/home/home.module').then(m=>m.HomeModule)
@@ -14,7 +16,12 @@ const routes: Routes = [
       path:'book',component:BooksComponent,
       loadChildren: () => import('./UI/components/books/books.module').then(m=>m.BooksModule)
     }
-  ]}
+  ]},
+  {
+    path:'login',
+    component:LoginComponent,
+    loadChildren: () => import('./UI/login/login/login.module').then(m=>m.LoginModule)
+  }
 ];
 
 @NgModule({
